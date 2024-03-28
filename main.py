cca8ce8f5e6bb8dd90108d06e851c14adc7e1df5
Philipp Jung <philipp.jung@mailbox.org>
2024-03-28 20:19:40 +0100

    Improve repo structure

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from ultralytics import YOLO
 
 
 if __name__ == "__main__":
     model = YOLO("yolov8n.yaml")
 
-    trained = model.train(data="config.yaml", epochs=3)
+    trained = model.train(data="config.yaml", epochs=1)
